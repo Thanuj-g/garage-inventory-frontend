c50cd6f2c7042002306f6d22eee4093b5f3aa6f0
Thanuj-g <thanujgayan2000@gmail.com>
2026-01-18 21:36:35 +0530

    Fix import path for PurchaseOrdersPage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Categories from "./pages/Categories";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import SalesAndUsagePage from "./pages/SalesAndUsagePage";
-
 import PurchaseOrdersPage from "./pages/purchesOrder";
 
 export default function App() {
